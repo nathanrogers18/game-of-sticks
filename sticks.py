add1c90e2556f4efcaa827bc456b50624e972d59
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-07-19 12:00:40 -0400

    Added 2 AI modes

--- sticks.py
+++ sticks.py
@@ -12,11 +12,11 @@ Repeat steps 3 and 4 until all tasks are implemented.
 Write a main() function with a game loop that uses your already tested and developed functionality in conjunction with getting user input and printing StickGameut.
 
 """
+import random
 
 class StickGame:
     """Creates new instance of a StickGame """
     def setup(self):
-        print("Welcome to the Game of Sticks!")
         while True:
             try:
                 sticks = int(input("How many sticks are on the table initially (10-100)? "))
@@ -29,7 +29,9 @@ class StickGame:
             else:
                 self.sticks = sticks
                 break
+
         self.player_turn = "Player 1"
+
 
     def take_sticks(self):
         """The current player takes sticks. """
@@ -44,10 +46,25 @@ class StickGame:
                 print("Please enter an integer between 1 and 3.")
                 continue
             else:
-                self.sticks_taken = sticks_taken
+                self.sticks -= sticks_taken
                 break
 
-        self.sticks -= self.sticks_taken
+    def take_sticks_ai(self):
+        """Choose a random number of sticks from the hat content,
+           and record the choice"""
+        sticks_taken = random.choice(self.ai.hats[self.sticks]['content'])
+        self.ai.hats[self.sticks]['choice'] = sticks_taken
+        self.sticks -= sticks_taken
+
+    def take_sticks_like_a_pro(self):
+        shit_sticks = 1
+        while shit_sticks < self.sticks - 4:
+            shit_sticks += 4
+        if self.sticks - shit_sticks <= 3:
+            sticks_taken = self.sticks - shit_sticks
+        else:
+            sticks_taken = 1
+        self.sticks -= sticks_taken
 
     def change_turn(self):
         if self.player_turn == "Player 1":
@@ -55,24 +72,74 @@ class StickGame:
         elif self.player_turn == "Player 2":
             self.player_turn = "Player 1"
 
-
-
-
-
     def __init__(self):
-        self.setup()
-        while self.sticks > 0:
-            self.take_sticks()
-            self.change_turn()
-        print("{} wins!".format(self.player_turn))
+        print("Welcome to the Game of Sticks!")
+        mode = input("'P' for Player vs. Player, 'S' for Player vs. Smart AI, 'Any Key' for Player vs. Slow Learning AI ").lower()
+        if mode == 'p':
+            print("You've selected Player vs. Player!")
+            self.mode = 'human'
+        elif mode == 's':
+            print("You've selected Player vs. Smart AI!")
+            self.mode = 'no fun'
+        else:
+            print("You've selected Player vs. Learning Slowly AI!")
+            self.mode = 'ai'
 
+        while True:
+            try:
+                sticks = int(input("How many sticks are on the table initially (10-100)? "))
+            except ValueError:
+                print("Please enter an integer between 10 and 100.")
+                continue
+            if sticks < 4 or sticks > 100:
+                print("Please enter an integer between 10 and 100.")
+                continue
+            else:
+                self.start_sticks = sticks
+                self.sticks = sticks
+                break
 
+        if self.mode == 'ai':
+            self.ai = AI(self.sticks)
 
+        while True:
+            self.sticks = self.start_sticks
+            self.player_turn = "Player 1"
+            while self.sticks > 0:
+                if self.mode == 'ai' and self.player_turn == "Player 2":
+                    self.take_sticks_ai()
+                    self.change_turn()
+                elif self.mode == 'no fun' and self.player_turn == "Player 2":
+                    self.take_sticks_like_a_pro()
+                    self.change_turn()
+                else:
+                    self.take_sticks()
+                    self.change_turn()
+            print("{} wins!".format(self.player_turn))
+
+            replay = input("'Y' to play again, Any Key to quit: ").lower()
+
+            if replay != 'y':
+                print("Thanks for playing!")
+                break
+
+class AI:
+    def __init__(self, sticks):
+        self.hats = {}
+        self.sticks = sticks
+        for i in range(1, self.sticks + 1):
+            self.hats[i] = {'content': [1,2,3], 'choice': None}
+
+    def __str__(self):
+        for keys, values in self.hats.items():
+            print("{}: {}".format(keys, values))
+        return __name__
 
 
 
 def main():
-    stick_game = StickGame()
+    StickGame()
+
 
 """
 Welcome to the Game of Sticks!
